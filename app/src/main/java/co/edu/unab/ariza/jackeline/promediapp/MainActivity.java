f4e522102f3e78c0d7de6be4397fb12fcee24baf
Aula L 5-4 <l54@academico.unab.ad>
2020-02-19 14:56:24 -0500

    segundo commit

---
+++ app/src/main/java/co/edu/unab/ariza/jackeline/promediapp/MainActivity.java
@@ -0,0 +1,14 @@
+package co.edu.unab.ariza.jackeline.promediapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_taller);
+    }
+}
