25e4c3a7edb3d1c10d8249e566f11da2b3a5d09a
Aula L 5-4 <l54@academico.unab.ad>
2020-02-24 15:50:01 -0500

    Implementación de POO

--- app/src/main/java/co/edu/unab/Martinez/Andrea/promediapp/ResultadoActivity.java
+++ app/src/main/java/co/edu/unab/Martinez/Andrea/promediapp/ResultadoActivity.java
@@ -3,12 +3,28 @@ package co.edu.unab.Martinez.Andrea.promediapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultadoActivity extends AppCompatActivity {
+
+    private TextView txtresultado;
+    private TextView txttitulo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resultado);
+
+        Bundle data = getIntent().getExtras();
+
+        double resultado = data.getDouble("resultado");
+        String titulo = data.getString("titulo");
+
+        txtresultado = findViewById(R.id.txt_resultado);
+        txttitulo = findViewById(R.id.txt_titulo);
+
+        txtresultado.setText(String.valueOf(resultado));
+        txttitulo.setText(titulo);
+
     }
 }
